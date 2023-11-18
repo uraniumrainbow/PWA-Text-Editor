5f6b19679e450965edcb45a7527985bc420b2898
uraniumrainbow <uraniumrainbow@gmail.com>
2023-11-18 14:02:27 -0700

    plugin files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,9 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const WorkboxPlugin = require("workbox-webpack-plugin");
 // TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = () => {
   return {
@@ -18,12 +20,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: ".src-sw.js",
+        swDest: "src-sw.js"
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "offline text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url:"/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.json()
+          },
+        ],
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test:/\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+            ],
+            },
+          },
+        },
       ],
     },
   };
