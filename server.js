19f61a40ef004e69f628cc0bf9082a7233cbace1
uraniumrainbow <uraniumrainbow@gmail.com>
2023-12-02 11:05:08 -0700

    server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static('./client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require('./server/routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
